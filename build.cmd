6aa45546e255b08ad2aa99cbd21fe6197d02942c
Jooseppi12 <urijozsef@europe.com>
2025-05-19 18:24:44 +0200

    Prep for nuget release

---
+++ build.cmd
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+    
+dotnet tool restore
+dotnet paket update -g wsbuild --no-install
+if errorlevel 1 exit /b %errorlevel%
+    
+dotnet fsi ./build.fsx -t %*
---
+++ build.fsx
@@ -0,0 +1,31 @@
+#r "nuget: FAKE.Core"
+#r "nuget: Fake.Core.Target"
+#r "nuget: Fake.IO.FileSystem"
+#r "nuget: Fake.Tools.Git"
+#r "nuget: Fake.DotNet.Cli"
+#r "nuget: Fake.DotNet.AssemblyInfoFile"
+#r "nuget: Fake.DotNet.Paket"
+#r "nuget: Paket.Core, 8.1.0-alpha004"
+#r "nuget: MSBuild.StructuredLogger"
+
+open Fake.Core
+System.Environment.GetCommandLineArgs()
+|> Array.skip 2 // skip fsi.exe; build.fsx
+|> Array.toList
+|> Fake.Core.Context.FakeExecutionContext.Create false __SOURCE_FILE__
+|> Fake.Core.Context.RuntimeContext.Fake
+|> Fake.Core.Context.setExecutionContext
+
+#load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open WebSharper.Fake
+
+let targets =
+    WSTargets.Default (fun () -> GetSemVerOf "WebSharper" |> ComputeVersion)
+    |> MakeTargets
+
+Target.runOrDefault "Build"
